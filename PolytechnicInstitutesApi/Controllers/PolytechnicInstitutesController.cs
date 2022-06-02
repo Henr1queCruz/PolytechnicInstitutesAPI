4f52d13e4bd2d85f21e1c3f0d77397f8abac23d8
Henr1queCruz <henriquebaptistacruz@gmail.com>
2022-06-02 22:32:00 +0100

    Initial commit

---
+++ PolytechnicInstitutesApi/Controllers/PolytechnicInstitutesController.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PolytechnicInstitutesApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PolytechnicInstitutesController : ControllerBase
+    {
+        IEnumerable<PolytechnicInstitutesDto> institutes = new List<PolytechnicInstitutesDto>();
+
+        private async Task<IEnumerable<PolytechnicInstitutesDto>> GetDataFromApiAsync()
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://universities.hipolabs.com/");
+                var result = await client.GetAsync("search?country=portugal");
+                if (result.IsSuccessStatusCode)
+                {
+                    institutes = await result.Content.ReadAsAsync<IEnumerable<PolytechnicInstitutesDto>>();
+                }
+                else //web api sent error response 
+                {
+                    //log response status here..
+
+                    institutes = Enumerable.Empty<PolytechnicInstitutesDto>();
+
+                    ModelState.AddModelError(string.Empty, "Server error. Please contact administrator.");
+                }
+            }
+            return institutes;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<PolytechnicInstitutes>>> MappingDataFromApi()
+        {
+            await GetDataFromApiAsync();
+            var institutesResult = institutes.Select(item => new PolytechnicInstitutes
+            {
+                Name = item.Name,
+                Webpage = item.Web_pages[0]
+            }).ToList();
+            return Ok(institutesResult);
+        }
+
+        [HttpGet("count")]
+        public async Task<ActionResult<int>> NumberOfELements()
+        {
+            await GetDataFromApiAsync();
+            var numberOfElements = institutes.Select(item => new PolytechnicInstitutes
+            {
+                Name = item.Name,
+                Webpage = item.Web_pages[0]
+            }).Count();
+            return Ok(numberOfElements);
+        }
+
+        [HttpGet("ascendingOrder")]
+        public async Task<ActionResult<IEnumerable<PolytechnicInstitutes>>> AscendingOrder()
+        {
+            await GetDataFromApiAsync();
+            var institutesResult = institutes.Select(item => new PolytechnicInstitutes
+            {
+                Name = item.Name,
+                Webpage = item.Web_pages[0]
+            }).OrderBy(i => i.Name).ToList();
+            return Ok(institutesResult);
+        }
+
+        [HttpGet("descendingOrder")]
+        public async Task<ActionResult<IEnumerable<PolytechnicInstitutes>>> DescendingOrder()
+        {
+            await GetDataFromApiAsync();
+            var institutesResult = institutes.Select(item => new PolytechnicInstitutes
+            {
+                Name = item.Name,
+                Webpage = item.Web_pages[0]
+            }).OrderByDescending(i => i.Name).ToList();
+            return Ok(institutesResult);
+        }
+    }
+}
---
+++ PolytechnicInstitutesApi/PolytechnicInstitutes.cs
@@ -0,0 +1,23 @@
+﻿
+namespace PolytechnicInstitutesApi
+{
+    public class PolytechnicInstitutes
+    {
+        public string Name { get; set; }
+        public string Webpage { get; set; }
+    }
+}
+
+namespace PolytechnicInstitutesApi
+{
+    public class PolytechnicInstitutesDto
+    {
+        public string Alpha_two_code { get; set; }
+        public string[] Domains { get; set; }
+        public string Country { get; set; }
+        public string[] Web_pages { get; set; }
+        public string Name { get; set; }
+    }
+}
+
+
